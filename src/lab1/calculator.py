cb777c29b1da38fcd82e2b5318c5b414327aeddb
SemYeat <semkarkovskii@gmail.com>
2025-08-18 17:38:49 +0300

    Добавлено решение

---
+++ src/lab1/calculator.py
@@ -0,0 +1,43 @@
+def calculator():
+    print("Операции")
+    print("+")
+    print("-")
+    print("*")
+    print("/")
+    print("^")
+    print("%")
+
+    while True:
+        oper = int(input())
+        if oper not in ["+", "-", "*", "/", "^", "%"]:
+            print("Неверная операция")
+            continue
+        try:
+            num1 = float(input("Введи первое число:"))
+            num2 = float(input("Введи второе число"))
+        except ValueError:
+            print("Не верно. Введите числа")
+            continue
+
+        if oper == "+":
+            rez = num1 + num2
+        elif oper == "-":
+            rez = num1 - num2
+        elif oper == "*":
+            rez = num1 * num2
+        elif oper == "/":
+            if num2 == 0:
+                print("Деление на ноль")
+                continue
+            else:
+                rez = num1 / num2
+        elif oper == "^":
+            rezz = num1**num2
+        elif oper == "%":
+            rez = num1 % num2
+
+        print(f" {num1} {oper} {num2} = {rez}")
+
+
+if __name__ == "__main__":
+    calculator()
